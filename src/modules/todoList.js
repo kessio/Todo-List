ad852fcd9e762721bf427d6e32cac43bc8b7a2fb
kessio <64524758+kessio@users.noreply.github.com>
2022-08-10 15:44:27 +0300

    created modules

---
+++ src/modules/todoList.js
@@ -0,0 +1,10 @@
+class TodoList{
+    constructor(description, completed, index) {
+        this.description = description;
+        this.completed = completed;
+        this.index = index;
+
+    }
+}
+
+export default TodoList;
